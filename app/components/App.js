6c304e7c8db234942a10f66b48f440a9ac9a65ca
Jake Ninness <jake.ninness@gmail.com>
2017-04-25 16:14:53 +1200

    Created navigation and routing

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,20 @@
 var React = require('react');
 const Popular = require('./popular');
+const ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+const Nav = require('./Nav');
 
 class App extends React.Component {
   render() {
     return(
-      <div className="container">
-        <Popular />
-      </div>
-    )
+      <Router>
+        <div className="container">
+          <Nav/>
+          <Route path='/popular' component={Popular} />
+        </div>
+      </Router>
+    );
   }
 }
 
