8c9ea00b6b1262d039b27620cfaecda0bb5e3df0
Jake Ninness <jake.ninness@gmail.com>
2017-04-25 16:33:45 +1200

    404 switch

--- app/components/App.js
+++ app/components/App.js
@@ -3,6 +3,7 @@ const Popular = require('./popular');
 const ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 const Nav = require('./Nav');
 const Home = require('./Home');
 const Battle = require('./Battle');
@@ -13,9 +14,14 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav/>
-          <Route exact path='/' component={Home} />
-          <Route  path='/battle' component={Battle} />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function(){
+              return <p>Page Not Found</p>;
+            }}/>
+          </Switch>
         </div>
       </Router>
     );
